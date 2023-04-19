6cce3709fe96ed7a50ad7ae4069dd506efdad880
SungYong Chung <sc4286@columbia.edu>
2023-04-19 22:07:21 +0900

    2023 04 19 javascript update

--- 09_javascript/register.js
+++ 09_javascript/register.js
@@ -4,6 +4,7 @@ var pw2 = document.querySelector("#user-pw2");
 
 userId.onchange = checkId;
 pw1.onchange = checkPw;
+pw2.onchange = comparePw;
 
 function checkId() {
 	if (userId.value.length < 4 || userId.value.length > 15) {
@@ -17,5 +18,13 @@ function checkPw() {
 		alert("More than 8.");
 		pw1.value = "";
 		pw1.focus();
+	}
+}
+
+function comparePw() {
+	if (pw1.value != pw2.value) {
+		alert("Diff PW.");
+		pw2.value = "";
+		pw2.focus();
 	}
 }
